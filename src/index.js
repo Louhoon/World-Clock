2f2bc6844e287ee700d383ec5e1399be1f0fd5c3
louhoon <louane.voisin@hotmail.com>
2024-02-03 21:45:13 +0100

    added possibility to show time of selected cities and added button to be back to main page

--- src/index.js
+++ src/index.js
@@ -1,34 +1,69 @@
 function updateTime() {
-	//london
-	let londonElement = document.querySelector("#london");
-	let londonTimeElement = document.querySelector("#london-time");
-	let londonTime = moment().tz("Europe/London");
-	
-	londonTimeElement.innerHTML = `${londonTime.format(
-		"h:mm:ss"
-	)} <small>${londonTime.format("A")}<small>`;
-	
-	//Sydney
-	let sydneyElement = document.querySelector("#sydney");
-	let sydneyTimeElement = document.querySelector("#sydney-time");
-	let sydneyTime = moment().tz("Australia/Sydney");
-	
-	sydneyTimeElement.innerHTML = `${sydneyTime.format(
-		"h:mm:ss"
-	)} <small>${sydneyTime.format("A")}<small>`;
-	
-	//istanbul
-	let istanbulElement = document.querySelector("#istanbul");
-	let istanbulTimeElement = document.querySelector("#istanbul-time");
-	let istanbulTime = moment().tz("Asia/Istanbul");
-	
-	istanbulTimeElement.innerHTML = `${istanbulTime.format(
-		"h:mm:ss"
-	)} <small>${istanbulTime.format("A")}<small>`;
+  //london
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonTimeElement = londonElement.querySelector("#london-time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonTimeElement.innerHTML = `${londonTime.format(
+      "hh:mm:ss"
+    )} <small>${londonTime.format("A")}<small>`;
+  }
+
+  //Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyTimeElement = sydneyElement.querySelector("#sydney-time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+
+    sydneyTimeElement.innerHTML = `${sydneyTime.format(
+      "hh:mm:ss"
+    )} <small>${sydneyTime.format("A")}<small>`;
+  }
+
+  //istanbul
+  let istanbulElement = document.querySelector("#istanbul");
+  if (istanbulElement) {
+    let istanbulTimeElement = istanbulElement.querySelector("#istanbul-time");
+    let istanbulTime = moment().tz("Asia/Istanbul");
+
+    istanbulTimeElement.innerHTML = `${istanbulTime.format(
+      "hh:mm:ss"
+    )} <small>${istanbulTime.format("A")}<small>`;
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let citiesElement = document.querySelector("#update-cities");
+
+  citiesElement.innerHTML = `
+						<ul class="cities">
+							<li class="city">${cityName}<span class="hour">${cityTime.format(
+    "hh:mm:ss"
+  )} <small>${cityTime.format("A")}</small></span></li>
+						</ul>`;
+}
+
+
+function displayHomeCities() {
+	let homecitiesElement = document.querySelector("#update-cities");
+	homecitiesElement.innerHTML = `<div class="displayed-cities" id="update-cities">
+				<ul class="cities">
+					<li class="city" id="london">London<span class="hour" id="london-time"></span></li>
+					<li class="city" id="sydney">Sydney<span class="hour" id="sydney-time"></span></li>
+					<li class="city" id="istanbul">Istanbul<span class="hour" id="istanbul-time"></span></li>
+				</ul>
+			</div>`
 }
 
 updateTime();
 setInterval(updateTime, 1000);
 
+let citiesSelectElement = document.querySelector("#cities");
+citiesSelectElement.addEventListener("change", updateCity);
 
-
+let homeMenuCityBtn = document.querySelector("#update-timezone-cities-btn");
+homeMenuCityBtn.addEventListener("click", displayHomeCities);
